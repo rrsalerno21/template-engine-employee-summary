a2b21ecc87f6ca57e0c191ce4c5efcb31d3d29a0
Rocky <rrsalerno21@gmail.com>
2020-06-08 12:09:24 -0700

    added this.role property

--- Develop/lib/Employee.js
+++ Develop/lib/Employee.js
@@ -4,6 +4,7 @@ class Employee {
         this.name = name;
         this.id = id;
         this.email = email;
+        this.role = "Employee";
     };
 
     getName() {
@@ -19,7 +20,7 @@ class Employee {
     };
 
     getRole() {
-        return 'Employee'
+        return this.role;
     };
 };
 
