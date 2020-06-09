88042fd4fe53310321414fc52912c4bbf8b7557a
Rocky <rrsalerno21@gmail.com>
2020-06-08 17:03:41 -0700

    added outputPath to fs.writeFile in init()

--- Develop/app.js
+++ Develop/app.js
@@ -188,7 +188,7 @@ async function init() {
     }
     console.log(employees);
     const teamHTML = render(employees);
-    fs.writeFile('team.html', teamHTML, (err) => {
+    fs.writeFile(outputPath, teamHTML, (err) => {
         if (err) throw err;
         console.log('Success');
     });
@@ -196,23 +196,3 @@ async function init() {
 }
 
 init();
-
-// After the user has input all employees desired, call the `render` function (required
-// above) and pass in an array containing all employee objects; the `render` function will
-// generate and return a block of HTML including templated divs for each employee!
-
-// After you have your html, you're now ready to create an HTML file using the HTML
-// returned from the `render` function. Now write it to a file named `team.html` in the
-// `output` folder. You can use the variable `outputPath` above target this location.
-// Hint: you may need to check if the `output` folder exists and create it if it
-// does not.
-
-// HINT: each employee type (manager, engineer, or intern) has slightly different
-// information; write your code to ask different questions via inquirer depending on
-// employee type.
-
-// HINT: make sure to build out your classes first! Remember that your Manager, Engineer,
-// and Intern classes should all extend from a class named Employee; see the directions
-// for further information. Be sure to test out each class and verify it generates an
-// object with the correct structure and methods. This structure will be crucial in order
-// for the provided `render` function to work! ```
